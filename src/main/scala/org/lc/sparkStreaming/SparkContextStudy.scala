830321e1202429a366768d8ab909a25f08929cbd
licheng <291215654@qq.com>
2017-01-22 16:04:54 +0800

    添加一个文件

--- src/main/scala/org/lc/sparkStreaming/SparkContextStudy.scala
+++ src/main/scala/org/lc/sparkStreaming/SparkContextStudy.scala
@@ -6,7 +6,8 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by licheng on 1/14/17.
   */
 object SparkContextStudy {
-  val conf = new SparkConf().
+
+  val conf = new SparkConf()
   val sc = new SparkContext()
 
 }
